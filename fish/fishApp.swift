4df8e0ea000e4f2aef7f9811c94f4bbfaea7eed6
Mary <chenxianhui.mary@gmail.com>
2025-11-08 12:58:59 +0800

    Initial Commit

---
+++ fish/fishApp.swift
@@ -0,0 +1,17 @@
+//
+//  fishApp.swift
+//  fish
+//
+//  Created by Event on 8/11/25.
+//
+
+import SwiftUI
+
+@main
+struct fishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
